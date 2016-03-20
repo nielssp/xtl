3135decab9648138ac560d7b04d3390cd181eb3f
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2016-03-20 12:29:02 +0100

    Update webpack configuration.

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,22 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
     context: __dirname + "/src/js",
-    entry: "./entry",
+    entry: "./main",
     output: {
         path: __dirname + "/dist",
         filename: "js/bundle.js"
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" },
-            { test: /\.scss$/, loader: "style!css!sass" },
-            { test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$/, loader: "file" }
+            { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader") },
+            { test: /\.scss$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader") },
+            { test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$/, loader: "file" },
+            { test: /\.html?$/, loader: "file?name=[name].[ext]" }
         ]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin("css/bundle.css", {
+            allChunks: true
+        })
+    ]
 };
