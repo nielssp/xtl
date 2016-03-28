cae5f36d7b054ca6bf7f50a49b11e897c16bbcdd
Niels Sonnich Poulsen <nielssonnich@gmail.com>
2016-03-28 17:58:28 +0200

    Type cases.

--- src/js/type.js
+++ src/js/type.js
@@ -18,14 +18,47 @@
 exports.Type = Type;
 exports.infer = infer;
 
-function Type(kind, value) {
-}
-
-function infer(node) {
+function infer(env, node) {
     switch (node.type) {
-        
+        case 'let-expression':
+            break;
+        case 'lambda-expression':
+            break;
+        case 'if-expression':
+            var cond = infer(env, node.children[0]);
+            var cons = infer(env.apply(cond.sub), node.children[1]);
+            var alt = infer(env.apply(cons.sub), node.children[2]);
+            var sub1 = cond.type.apply(alt.sub).unify('bool');
+            var sub2 = cons.type.apply(sub1).unify(alt.apply(sub1));
+            return {type: alt.type, sub: compose(alt.sub, sub1, sub2)};
+        case 'app-expression':
+            break;
+        case 'name':
+            break;
+        case 'number':
+            return {type: 'number', sub: {}};
+        case 'string':
+            return {type: 'string', sub: {}};
+        case 'placeholder':
+            break;
     }
 }
+
+function Type(kind, value) {
+    this.kind = kind;
+    this.value = value;
+    
+    this.children = [];
+}
+
+function TypeScheme(names, type) {
+    
+}
+
+function TypeEnv(map) {
+    
+}
+
 
 function apply(type, substitution) {
     switch (type.kind) {
